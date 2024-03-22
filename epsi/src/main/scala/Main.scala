80527b03758814f50554fa6f33ce1031e27e5959
marcopyre <marco.pyre@epsi.fr>
2024-03-22 12:02:25 +0100

    nullpointer

--- epsi/src/main/scala/Main.scala
+++ epsi/src/main/scala/Main.scala
@@ -1,43 +1,42 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import scala.collection.mutable.StringBuilder
+import org.apache.spark.sql.SparkSession
+
+import org.apache.spark.ml.Pipeline
+import com.johnsnowlabs.nlp.pretrained.PretrainedPipeline
+import com.johnsnowlabs.nlp.Finisher
+
+
 
 object Main extends App {
-  val conf = new SparkConf().setAppName("epsi").setMaster("local")
-  val sc = new SparkContext(conf)
+  val sc: SparkSession =
+  SparkSession
+    .builder()
+    .appName("epsi")
+    .config("spark.master", "local")
+    .getOrCreate()
 
-  val fileRdd1 = sc.textFile("./books_large_p1.txt")
-  val fileRdd2 = sc.textFile("./books_large_p2.txt")
-  val fileRdd = fileRdd1.union(fileRdd2)
+  import sc.implicits._
+
+  val fileRdd1 = sc.sparkContext.textFile("./books_large_p1.txt")
+  val fileRdd2 = sc.sparkContext.textFile("./books_large_p2.txt")
+  val fileRdd = fileRdd1.union(fileRdd2).zipWithIndex
   var skip = false
-  var memory = ""
-  var Books:Map[String,String] = Map()
+  var prevIndex = 0
+  val bookIndexs = fileRdd.filter(line => line._1 contains "isbn : ")
 
-  val sb = new StringBuilder()
+  val explainPipelineModel = PretrainedPipeline("explain_document_ml").model
+  val finisherExplainEn = new Finisher().setInputCols("token", "lemmas", "pos")
+  val pipelineExplainEn = new Pipeline().setStages(Array(explainPipelineModel,finisherExplainEn))
 
-  fileRdd.foreach(line => {
-    if(line contains "isbn : "){
-      if(skip == false){
-        var isbn = ""
-        var running = true
-        for(lettre <- line.substring(line.indexOf("isbn : ") + 7); if running){
-          if(!lettre.isSpaceChar){
-            isbn += lettre.toString
-          }
-          else{
-            running = false
-          }
-        }
-        println(isbn)
-        //println(sb.toString())
-        sb.setLength(0)
-      }
-      else{
-        skip = true
-      }
-    }
-    sb.append(line.toString);
-    //memory = memory.concat(line.toString)
+  bookIndexs.foreach(each => {
+    val output = fileRdd.filter(line => line._2 > prevIndex && line._2 <= each._2.toInt).map(line => line._1)
+    val ds = output.toDS()
+    val dsRenamed = ds.withColumnRenamed("value", "text")
+
+    val modelExplainEn = pipelineExplainEn.fit(dsRenamed)
+    val annoteTexteEn = modelExplainEn.transform(dsRenamed).cache()
+    annoteTexteEn.show()
+    prevIndex = each._2.toInt
   })
+
   sc.stop()
 }
